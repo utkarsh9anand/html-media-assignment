5964c59a041d584f507c18522356e4544972394b
utkarsh9anand <83171168+utkarsh9anand@users.noreply.github.com>
2023-09-27 12:57:42 +0530

    Add files via upload

---
+++ q5_differenceBWascaii.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char x,y;
+    cout<<"enter the first character ";
+    cin>>x;
+    cout<<"enter the second character ";
+    cin>>y;
+    cout<<"the difference between two characters are "<<int(x)-int(y)<<endl;
+
+}
